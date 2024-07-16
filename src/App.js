279d8d173b4fb3eac8ec100bc75f39a93b46dd4f
Angelica <puchetatome.angelica@gmail.com>
2024-07-15 19:02:40 -0600

    imported CSS file to in file App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+//Importa el CSS de Bootstrap
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+// Importa el JS de Bootstrap
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function App() {
   return (
