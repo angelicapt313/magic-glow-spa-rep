8608d90d9a5c6c7e060d6a4d5d85d02d11637873
Angelica <puchetatome.angelica@gmail.com>
2024-07-18 15:11:20 -0600

    Changed the element “a” for “Link".

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import cosmetico from '../assets/images/cosmetico.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <Link to="/" className="navbar-brand" href="#">
                     <img alt='Cosmetico png' src={cosmetico} className='mx-2' style={{width:'30px', height:'30px'}} />
-                    Magic Glow
-                </a>
+                    Magic Glow Spa
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
+                            <Link to="/" className="nav-link" href="#">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Reservation</a>
+                            <Link to="/" className="nav-link" href="#">Reservation</Link>
                         </li>
                     </ul>
                 </div>
