d779b40dce8fa9836635968a0b81a0e1cc79a772
Angelica <puchetatome.angelica@gmail.com>
2024-07-20 13:29:52 -0600

    Added some styles to the header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import cosmetico from '../assets/images/cosmetico.png'
 import { Link } from 'react-router-dom';
+import '../styles/header.css'
 
 const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <Link to="/" className="navbar-brand" href="#">
+                <Link to="/" className="navbar-brand magic-glow-spa">
                     <img alt='Cosmetico png' src={cosmetico} className='mx-2' style={{width:'30px', height:'30px'}} />
                     Magic Glow Spa
                 </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link to="/" className="nav-link" href="#">Services</Link>
+                            <Link to="/" className="nav-link">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/" className="nav-link" href="#">Reservation</Link>
+                            <Link to="/" className="nav-link">Reservation</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
     )
 }
 
