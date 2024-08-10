c6ce4d32da1bcf3465c72128a3c5ba77a0148c65
Angelica <puchetatome.angelica@gmail.com>
2024-08-10 15:32:42 -0600

    Delete the links from the navbar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,27 +9,14 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <Link to="/" className="navbar-brand magic-glow-spa">
-                    <img alt='Cosmetico png' src={cosmetico} className='mx-2' style={{width:'30px', height:'30px'}} />
+                    <img alt='Cosmetico png' src={cosmetico} className='mx-2' style={{ width: '30px', height: '30px' }} />
                     Magic Glow Spa
                 </Link>
-                <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/services" className="nav-link">Services</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/reservation" className="nav-link">Reservation</Link>
-                        </li>
-                    </ul>
+                <div className="flex">
+                    <Link to="/cart">
+                        <img src={imgCart} />
+                    </Link>
                 </div>
-            <div className="flex">
-                <Link to="/cart">
-                <img src={imgCart}/>
-                </Link>
-            </div>
             </div>
         </nav>
     )
