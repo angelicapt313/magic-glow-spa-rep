cf38c413359d55884267e314083ddd3e4721f55f
Angelica <puchetatome.angelica@gmail.com>
2024-08-09 21:56:23 -0600

    Created a context for the cart component.

---
+++ src/context/CartContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from "react";
+
+export const  CartContext = createContext();
+
+const CartProvider = ({children}) => {
+
+    const [cart, setCart] = useState([]);
+
+    const addShopCart = (product) => {
+        setCart((prevCart) => {
+            const productExists = prevCart.find(item => item.ProductID === product.ProductID);
+
+            if (productExists) {
+                if (product.quantityAdded > 0) {
+                    return prevCart.map(item => item.ProductID === product.ProductID ? { ...item, quantityAdded: product.quantityAdded } : item
+                    );
+                } else {
+                    return prevCart.filter(item => item.ProductID !== product.ProductID);
+                }
+
+            }
+            return [...prevCart, product,];
+        });
+    };
+
+    const removeFromCart = (productId) => {
+        setCart((prevCart) => prevCart.filter(item => item.ProductID !== productId));
+    }
+
+    return (
+        <CartContext.Provider value={{ cart, addShopCart, removeFromCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
