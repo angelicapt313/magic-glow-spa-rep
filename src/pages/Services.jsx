f73c22a84f21d8b46c8f847725d7acb43c4446f5
Angelica <puchetatome.angelica@gmail.com>
2024-08-11 20:30:01 -0600

    Loading data from the JSON file.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import manicure from '../assets/images/manicure.png';
 import pedicure from '../assets/images/pedicure.png';
 import makeUp from '../assets/images/makeUp.png';
@@ -12,84 +12,32 @@ import '../styles/services.css'
 
 
 const Services = () => {
+    const [servicesList, setServicesList] = useState([]);
+
+    useEffect( () => {
+        const fetchServices = async () => {
+            try{
+                const response = await fetch('/services.json');
+                const data = await response.json();
+                setServicesList(data);
+            } catch(error) {
+                console.log('Error fetching the services data: ', error);
+            }
+        };
+
+        fetchServices();
+
+    }, []);
+
     return (
-        <div className="content vh-100">
-            <h3 className="text-center m-4 services  ">Delight in our services.</h3>
-            <div className="row mx-2 row-gap-4">
-                <div className="col-6 col-md-4 col-lg-3" >
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }} >
-                        <div className="card-body text-center">
-                            <img src={manicure} alt="Manicure" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Manicure</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={pedicure} alt="Pedicure" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Pedicure</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={makeUp} alt="Make Up" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Make Up</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={hairStyle} alt="Hairstyle" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Hairstyle</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={hairCutting} alt="Hair Cutting" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Hair Cutting</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={eyebrow} alt="Eyebrow Design" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Eyebrow Design</p>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={depilation} alt="Depilation" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Depilation</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-6 col-md-4 col-lg-3">
-                    <div className="card card-services" style={{ width: "100%", height: "100%" }}>
-                        <div className="card-body text-center">
-                            <img src={cleaning} alt="Skin Cleaning" style={{ width: "100px" }} />
-                            <p className="card-title p-2 m-1 fw-bold">Skin Cleaning</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+        <div>
+         <h3 className="text-center m-4 services">Delight in our services.</h3>
+         <ul>
+            {servicesList.map(item => (
+             <li key={item.id}>{item.name}</li>             
+            ))
+            }
+         </ul>
         </div>
 
     )
