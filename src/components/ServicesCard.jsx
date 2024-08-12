0c9a20b18a423de9aeeb903dfe86852389de88ae
Angelica <puchetatome.angelica@gmail.com>
2024-08-12 17:46:52 -0600

    Added neumorphism styles to the add button.

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import '../styles/servicesCard.css';
 
 const ServicesCard = ({ service }) => {
-    
+
     const [quantity, setQuantity] = useState(0);
- 
+
 
     return (
-        <div className=" card-css p-1 m-2 text-center" >
-            <div className="card-body">
-                <img src={(`../${service.image}`)} className="w-75" alt={service.name}/>
+        <div className="card-css p-1 my-3 text-center flex" >
+            <div className="card-body ">
+                <img src={(`../${service.image}`)} className="w-75" alt={service.name} />
                 <p className="card-title fw-bolder">{service.name}</p>
-                <p className="card-price fw-bold">${service.price}</p>
+
+                <div className="button d-flex justify-content-end align-items-end">
+                    <p className="card-price fw-bolder mx-2 mb-0">${service.price}</p>
+                    <button className="btn btn-add text-center">+</button>
+                </div>
             </div>
+
+
         </div>
+
+
 
 
     );
