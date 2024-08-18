e557ec28cc06ebbd471fb0dcf0c5fe05512bd1e5
Angelica <puchetatome.angelica@gmail.com>
2024-08-17 21:10:38 -0600

    Added some styles to the modal.

--- src/components/InfoMondal.jsx
+++ src/components/InfoMondal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from "./Form";
+import '../styles/InfoModal.css';
 
 function InfoModal({ service, closeModal }) {
 
@@ -8,23 +9,23 @@ function InfoModal({ service, closeModal }) {
 
   return (
     <div className="modal show d-block" tabIndex="-1" role="dialog">
-      <div className="modal-dialog" role="document">
+      <div className="modal-dialog modal-dialog-centered rounded-bottom-pill" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">Confirmar Agregar</h5>
+            <h5 className="modal-title text-body-secondary">Completa la siguiente información.</h5>
             <button type="button" className="btn-close" onClick={closeModal}></button>
           </div>
           <div className="modal-body">
             <Form selectedService={service} />
           </div>
-          <div className="modal-footer">
+          {/* <div className="modal-footer">
             <button type="button" className="btn btn-secondary" onClick={closeModal}>
               Cancelar
             </button>
             <button type="button" className="btn btn-primary" onClick={closeModal}>
               Confirmar
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
