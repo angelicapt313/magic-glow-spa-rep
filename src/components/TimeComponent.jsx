7cd65454f8fe4048b605e94b46e26e08980cc9c6
Angelica <puchetatome.angelica@gmail.com>
2024-10-02 14:39:54 -0600

    Save all the form fields to make a reservation.

--- src/components/TimeComponent.jsx
+++ src/components/TimeComponent.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-// import 'react-time-picker/dist/TimePicker.css';
-// import 'react-clock/dist/Clock.css'
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css'
 import TimePicker from "react-time-picker";
+import '../styles/timeComponent.css'
 
-const TimeComponent = ({ selectedTime, onTimeChange } ) => {
+const TimeComponent = ({ selectedTime, onTimeChange }) => {
 
     return (
-        <div className="time-picker-container">
-        <p>Selecciona Horario</p>
-        <TimePicker
-        // value={value}
-        // onChange={(newValue) => setValue(newValue)}
-        onChange={onTimeChange}
-        value={selectedTime}
-        format="hh:mm a"
-        clearIcon={null}
-        />
+        <div className="position-relative">
+            <p className="fw-bold">Selecciona un Horario:</p>
+            <div className="time-picker-container w-100 border-4">
+                <TimePicker
+                    // value={value}
+                    // onChange={(newValue) => setValue(newValue)}
+                    value={selectedTime}
+                    onChange={onTimeChange}
+                    format="hh:mm a"
+                    clearIcon={null}
+                    clockIcon={null}
+                    clockProps={{
+                        size: 130, // Tamaño del reloj
+                        hourHandWidth: 4, // Grosor de la aguja de las horas
+                        minuteHandWidth: 2, // Grosor de la aguja de los minutos
+                        renderNumbers: true, // Muestra los números en el reloj
+                        renderMinuteMarks: true, // Muestra las marcas de minutos
+
+                    }}
+                />
+            </div>
         </div>
+
     );
 }
 
