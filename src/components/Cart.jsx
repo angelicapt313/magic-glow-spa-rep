f84510fa441c9507eebb6da69db95bdc9ca93414
Angelica <puchetatome.angelica@gmail.com>
2024-11-25 15:47:57 -0600

    Added overflow to the cart component and removed the footer of the card.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,58 +9,44 @@ function Cart({ toggleCart }) {
   // const grandTotal = cart.reduce((acc, product) => acc + product.ProductPrice * product.quantityAdded, 0);
 
   return (
-    <>
 
-      <div className="cart-overlay d-flex justify-content-center align-items-center">
-        <div className="cart-container d-flex flex-column vh-100 position-relative ms-auto">
-          <button className="btn-close rounded-circle p-2" onClick={toggleCart}></button>
-          <div className="cart-list w-70 bg-white p-4 flex-grow-1">
-            {cartData.length === 0 ? (
-              <p className="text-center fw-bold">No hay servicios en el carrito.</p>
-            ) : (
-              cartData.map((appointment, index) => (
-                
-                <div className="card card-effect my-2" key={index}>
-                  
-                  <div className="card-body d-flex justify-content-evenly align-items-center ">
-                    <img src={appointment.image} className='image-service' alt='Cosmetico.png' />
 
-                    <ul className="list-unstyled align-self-center m-0">
+    <div className="cart-overlay d-flex justify-content-center align-items-center">
+      <div className="cart-container d-flex flex-column vh-100 position-relative ms-auto">
+        <button className="btn-close rounded-circle p-2" onClick={toggleCart}></button>
+        <div className="cart-list w-70 bg-white p-4 flex-grow-1 overflow-auto">
+          {cartData.length === 0 ? (
+            <p className="text-center fw-bold">No hay servicios en el carrito.</p>
+          ) : (
+            cartData.map((appointment, index) => (
 
-                      <li className="">
-                        <p className='m-0 p-1'> {appointment.fullName}</p>
-                      </li>
-                      <li className="">
-                        <p className='m-0 p-1 text-capitalize'>{new Date(appointment.selectedDate).toLocaleDateString('es-Es', {
-                          day: 'numeric',
-                          month: 'short',
-                          year: '2-digit'
-                        })} </p>
-                      </li>
-                      <li>
-                        <p>{appointment.selectedTime}hrs</p>
-                      </li>
-                    </ul>
-                  </div>
-                  
+              <div className="card card-effect my-2" key={index}>
+                <div className="card-body d-flex justify-content-evenly align-items-center ">
+                  <img src={appointment.image} className='image-service' alt='Cosmetico.png' />
+
+                  <ul className="list-unstyled align-self-center m-0">
+
+                    <li className="">
+                      <p className='m-0 p-1'> {appointment.fullName}</p>
+                    </li>
+                    <li className="">
+                      <p className='m-0 p-1 text-capitalize'>{new Date(appointment.selectedDate).toLocaleDateString('es-Es', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: '2-digit'
+                      })} </p>
+                    </li>
+                    <li>
+                      <p>{appointment.selectedTime}hrs</p>
+                    </li>
+                  </ul>
                 </div>
-
-              ))
-
-            )}
-
-
-
-          </div>
-          <div className="cart-footer bg-dark bg-opacity-75 p-3 text-center text-white">
-            <button className="btn">MGS</button>
-          </div>
+              </div>
+            ))
+          )}
         </div>
-
       </div>
-
-
-    </>
+    </div>
   );
 
 }
