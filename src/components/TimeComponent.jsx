fc7b831606bd1c21ca8e93dc0f3ada01dac5bd6a
Angelica <puchetatome.angelica@gmail.com>
2024-12-31 16:47:38 -0600

    Created a button to display the clock and to select the time.

--- src/components/TimeComponent.jsx
+++ src/components/TimeComponent.jsx
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import 'react-time-picker/dist/TimePicker.css';
 import 'react-clock/dist/Clock.css'
 import TimePicker from "react-time-picker";
 import '../styles/timeComponent.css'
 
 const TimeComponent = ({ selectedTime, onTimeChange }) => {
+
+    const [showTime, setShowTime] = useState(false);
+
     const handleTimeChange = (newTime) => {
         onTimeChange(newTime);
     }
 
-    return (
-        <div className="position-relative">
-            <p className="fw-bold">Selecciona un Horario:</p>
-            <div className="time-picker-container w-100 border-4">
-                <TimePicker
-                    // value={value}
-                    // onChange={(newValue) => setValue(newValue)}
-                    value={selectedTime}
-                    onChange={handleTimeChange}
-                    format="hh:mm a"
-                    clearIcon={null}
-                    clockIcon={null}
-                    clockProps={{
-                        size: 130, // Tamaño del reloj
-                        hourHandWidth: 4, // Grosor de la aguja de las horas
-                        minuteHandWidth: 2, // Grosor de la aguja de los minutos
-                        renderNumbers: true, // Muestra los números en el reloj
-                        renderMinuteMarks: true, // Muestra las marcas de minutos
+    const toggleTime = () => setShowTime(!showTime);
 
-                    }}
-                />
-            </div>
+    return (
+        <div className="wrapper">
+
+            <button type="button" onClick={toggleTime} className="btn" style={{ backgroundColor: '#b4a4bc' }}>
+                <p className="m-0">Seleccionar horario:</p>
+            </button>
+
+            <div className="mt-1">
+                {showTime && (
+                    
+                    <TimePicker
+                        className="bg-transparent border-1"
+                        value={selectedTime}
+                        onChange={handleTimeChange}
+                        format="hh:mm a"
+                        clearIcon={null}
+                        clockIcon={null}
+                        clockProps={{
+                            size: 130, // Tamaño del reloj
+                            hourHandWidth: 4, // Grosor de la aguja de las horas
+                            minuteHandWidth: 2, // Grosor de la aguja de los minutos
+                            renderNumbers: true, // Muestra los números en el reloj
+                            renderMinuteMarks: true, // Muestra las marcas de minutos
+
+                        }}
+                    />
+                )}
+                <p>Horario seleccionado: <span className="fw-bold">{selectedTime} hrs</span></p>
+                
+           </div>
         </div>
+
 
     );
 }
