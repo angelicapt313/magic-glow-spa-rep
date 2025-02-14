fe693c1cd90d2972784a647a90aceb1c2d24fe7a
Angelica <puchetatome.angelica@gmail.com>
2025-02-14 16:10:12 -0600

    I replaced the Bootstrap classes with TailwindCSS classes.

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -4,21 +4,28 @@ import '../styles/servicesCard.css';
 const ServicesCard = ({ service, openModal }) => {
 
     return (
-        <div className="card-css p-1 my-3 text-center flex" >
-            <div className="card-body ">
-                <img src={(`../${service.image}`)} className="w-75" alt={service.name} />
-                <p className="card-title fw-bolder">{service.name}</p>
+        <div className="w-full sm:w-[250px] h-auto rounded-2xl bg-transparent shadow-lg backdrop-blur-md 
+                        transition-transform hover:bg-dark-purple hover:translate-y-[-5px] p-2 flex flex-col justify-between">
 
-                <div className="button d-flex justify-content-end align-items-end">
-                    <p className="card-price fw-bolder mx-2 mb-0">${service.price}</p>
-                    <button onClick={openModal} className="btn btn-add text-center">+</button>
-                </div>
+            <div className="flex flex-col items-center">
+                <img src={`../${service.image}`} className="w-[75%] rounded-lg" alt={service.name} />
+                <p className="text-black font-bold text-lg">
+                    {service.name}
+                </p>
             </div>
 
-
+            <div className="flex justify-end items-center gap-2">
+                <p className="text-gray-500 font-semibold text-sm">
+                    ${service.price}
+                </p>
+                <button
+                    onClick={openModal}
+                    className="bg-custom-purple bg-gray-300 rounded-lg shadow-inner cursor-pointer text-lg px-3 py-1 
+                               transition-all hover:text-black hover:border-none">
+                    +
+                </button>
+            </div>
         </div>
-
-
 
 
     );
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,7 +5,7 @@ import ServicesCard from "../components/ServicesCard";
 const Services = ({ openModal }) => {
     const [servicesList, setServicesList] = useState([]);
 
-  
+
 
     useEffect(() => {
         const fetchServices = async () => {
@@ -24,23 +24,24 @@ const Services = ({ openModal }) => {
     }, []);
 
     return (
-        <div>
-            <h3 className="text-center m-4 services">Delight in our services.</h3>
+        <div className="flex flex-col items-center justify-center min-h-screen px-4">
+            <h3 className="text-lg sm:text-xl md:text-2xl text-gray-800 font-semibold text-center my-6">
+                Delight in our services.
+            </h3>
 
-            <div className="row justify-content-center">
-                {servicesList.map((service) => (
-                    <ServicesCard
-                        key={service.id}
-                        service={service}
-                        className="col col-md-4"
-                        openModal={() => openModal(service)}
+            <div className="flex justify-center w-full max-w-7xl">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-center gap-4">
+                    {servicesList.map((service) => (
+                        <ServicesCard
+                            key={service.id}
+                            service={service}
+                            openModal={() => openModal(service)}
                         />
-                ))
-                }
+                    ))}
+                </div>
             </div>
-          
-
         </div>
+
 
     )
 }
