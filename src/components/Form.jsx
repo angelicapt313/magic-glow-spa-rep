98749bdaef226a778eab616e6f180cec0475c286
Angelica <puchetatome.angelica@gmail.com>
2025-02-14 17:45:09 -0600

    I replaced the Bootstrap classes with TailwindCSS classes.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,132 +10,140 @@ const Form = ({ control, selectedService, selectedDate, setSelectedDate, selecte
 
     return (
         <>
-            <div className="container h-100 mt-1">
-                <form>
-                    <div className="gap-3 input-group">
-                        <div className="mb-3 col-5">
-                            <label htmlFor="fullName" className="form-label">Nombre</label>
+    
+            <div className="max-w-2xl mx-auto px-4 py-6">
+                <form className="space-y-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
+                                Nombre
+                            </label>
                             <Controller
                                 name="fullName"
                                 control={control}
                                 defaultValue={cartData[0]?.fullName || ""}
-                                render={({ field }) =>
-                                    <input type="text"
-                                        className="form-control"
+                                render={({ field }) => (
+                                    <input
+                                        type="text"
                                         id="fullName"
+                                        className="mt-1 block w-full rounded-md "
                                         placeholder="Escribe tu nombre completo"
                                         {...field}
                                         required
-                                    />}
-
+                                    />
+                                )}
                             />
                         </div>
 
-                        <div className="mb-3 col-6">
-                            <label htmlFor="email" className="form-label">Correo Electrónico</label>
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                                Correo Electrónico
+                            </label>
                             <Controller
                                 name="email"
                                 control={control}
                                 defaultValue={cartData[0]?.email || ""}
-                                render={({ field }) =>
-                                    <input type="email"
-                                        className="form-control"
+                                render={({ field }) => (
+                                    <input
+                                        type="email"
                                         id="email"
+                                        className="mt-1 block w-full rounded-md"
                                         placeholder="tucorreo@ejemplo.com"
-                                        {...field} />}
-                                required
-
+                                        {...field}
+                                        required
+                                    />
+                                )}
                             />
                         </div>
                     </div>
 
-                    <div className=" gap-3 input-group">
-                        <div className="mb-3 col-5">
-                            <label htmlFor="phone" className="form-label">Número de Teléfono</label>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+                                Número de Teléfono
+                            </label>
                             <Controller
                                 name="phone"
                                 control={control}
                                 defaultValue={cartData[0]?.phone || ""}
-                                render={({ field }) =>
-                                    <input type="number"
-                                        className="form-control"
+                                render={({ field }) => (
+                                    <input
+                                        type="number"
                                         id="phone"
+                                        className="mt-1 block w-full rounded-md "
                                         placeholder="(123) 456-7890"
                                         {...field}
                                         required
-                                    />}
-
+                                    />
+                                )}
                             />
                         </div>
 
-                        <div className="mb-3 col-5">
-                            <label htmlFor="service" className="form-label">Servicio Solicitado</label>
+                        <div>
+                            <label htmlFor="service" className="block text-sm font-medium text-gray-700">
+                                Servicio Solicitado
+                            </label>
                             <Controller
                                 name="service"
                                 control={control}
                                 defaultValue={selectedService.name || ""}
-                                render={({ field }) =>
-                                    <input type="text"
-                                        className="form-control fw-medium"
+                                render={({ field }) => (
+                                    <input
+                                        type="text"
                                         id="service"
+                                        className="mt-1 block w-full rounded-md"
                                         {...field}
                                         readOnly
-                                    />}
-
-
+                                    />
+                                )}
                             />
                         </div>
                     </div>
 
-
-                    <div className="mb-3">
-                        <label htmlFor="comments" className="form-label">Comentarios o Solicitudes Especiales</label>
+                    <div>
+                        <label htmlFor="comments" className="block text-sm font-medium text-gray-700">
+                            Comentarios o Solicitudes Especiales
+                        </label>
                         <Controller
                             name="comments"
                             control={control}
                             defaultValue={cartData[0]?.comments || ""}
-                            render={({ field }) =>
-                                <textarea className="form-control"
+                            render={({ field }) => (
+                                <textarea
                                     id="comments"
                                     rows="2"
+                                    className="mt-1 block w-full rounded-md"
                                     placeholder="Escribe aquí cualquier solicitud o comentario especial"
-                                    {...field}>
-                                </textarea>}
-
+                                    {...field}
+                                ></textarea>
+                            )}
                         />
                     </div>
 
-                    <div className="mb-3">
+                    <div>
                         <Controller
                             name="selectedDate"
                             control={control}
                             defaultValue={selectedDate}
                             render={({ field }) => (
-                                <CalendarComponent
-                                    selectedDate={field.value}
-                                    onDateChange={field.onChange} />
+                                <CalendarComponent selectedDate={field.value} onDateChange={field.onChange} />
                             )}
                         />
                     </div>
 
-                    <div className="mb-3">
+                    <div>
                         <Controller
                             name="selectedTime"
                             control={control}
                             defaultValue={selectedTime}
                             render={({ field }) => (
-                                <TimeComponent
-                                    selectedTime={field.value}
-                                    onTimeChange={field.onChange}
-                                />
+                                <TimeComponent selectedTime={field.value} onTimeChange={field.onChange} />
                             )}
                         />
                     </div>
+                </form>
+            </div>
 
-
-
-                </form >
-            </div >
         </>
     )
 }
