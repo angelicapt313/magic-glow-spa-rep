0f3cee2d1d9b1818357f2f39e9669b0683c18fe7
Angelica <puchetatome.angelica@gmail.com>
2025-03-06 14:13:19 -0600

    Uninstall the Bootstrap package from the project.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import './App.css';
-//Importa el CSS de Bootstrap
-import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
-// Importa el JS de Bootstrap
-import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
